258ef6ae7673cad3bc8869348fde03578549ceab
Andrzej Lobodzki <a-alobodzki@hotels.com>
2018-04-23 14:56:03 +0100

    Moving classes not related to client to proxy module

--- components/proxy/src/main/java/com/hotels/styx/applications/BackendServices.java
+++ components/proxy/src/main/java/com/hotels/styx/applications/BackendServices.java
@@ -13,7 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
  */
-package com.hotels.styx.client.applications;
+package com.hotels.styx.applications;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
