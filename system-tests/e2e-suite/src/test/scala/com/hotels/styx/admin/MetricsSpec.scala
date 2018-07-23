6fbade59de19ac9846c046798f47f10137457aad
Mikko Karjalainen <mikko.e.karjalainen@gmail.com>
2018-07-23 16:53:20 +0100

    Add admin endpoint for querying specific metrics by name (#206) (#218)
    (cherry picked from commit c9ef1a9)

---
+++ system-tests/e2e-suite/src/test/scala/com/hotels/styx/admin/MetricsSpec.scala
@@ -0,0 +1,53 @@
+/*
+  Copyright (C) 2013-2018 Expedia Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+package com.hotels.styx.admin
+
+import java.nio.charset.StandardCharsets.UTF_8
+
+import com.hotels.styx.api.FullHttpRequest.get
+import com.hotels.styx.api.messages.HttpResponseStatus.{NOT_FOUND, OK}
+import com.hotels.styx.infrastructure.HttpResponseImplicits
+import com.hotels.styx.{DefaultStyxConfiguration, StyxClientSupplier, StyxProxySpec}
+import org.scalatest.FunSpec
+
+
+class MetricsSpec extends FunSpec
+  with StyxProxySpec
+  with DefaultStyxConfiguration
+  with StyxClientSupplier
+  with HttpResponseImplicits {
+
+  describe("health check") {
+    it("exposes metrics beginning with prefix") {
+      val response = decodedRequest(get(styxServer.adminURL("/admin/metrics/jvm.bufferpool.direct")).build())
+      assert(response.status == OK)
+      assert(response.isNotCacheAble())
+      response.bodyAs(UTF_8) should include regex "\\{\"jvm.bufferpool.direct.used\":\\{\"value\":\\d+\\},\"jvm.bufferpool.direct.capacity\":\\{\"value\":\\d+\\},\"jvm.bufferpool.direct.count\":\\{\"value\":\\d+\\}\\}"
+    }
+
+    it("exposes metrics by exact name") {
+      val response = decodedRequest(get(styxServer.adminURL("/admin/metrics/jvm.bufferpool.direct.used")).build())
+      assert(response.status == OK)
+      assert(response.isNotCacheAble())
+      response.bodyAs(UTF_8) should include regex "\\{\"jvm.bufferpool.direct.used\":\\{\"value\":\\d+\\}\\}"
+    }
+
+    it("returns 404 for non-existent metrics") {
+      val response = decodedRequest(get(styxServer.adminURL("/admin/metrics/jvm.bufferpool.direct.used.foo.bar")).build())
+      assert(response.status == NOT_FOUND)
+    }
+  }
+}
