e7bf1b89377d567382d0f6d3508285883d0bcaf8
David Latorre <dvlato@gmail.com>
2018-11-30 08:47:35 +0000

    Version metric should be a string instead of an integer. (#352)

--- components/proxy/src/main/java/com/hotels/styx/Version.java
+++ components/proxy/src/main/java/com/hotels/styx/Version.java
@@ -23,10 +23,8 @@ import org.slf4j.Logger;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Objects;
-import java.util.Optional;
 
 import static com.google.common.base.Objects.toStringHelper;
-import static java.lang.Integer.parseInt;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -76,31 +74,7 @@ public class Version {
         return releaseTag.substring(firstDot + 1);
     }
 
-    public Optional<Integer> buildNumber() {
-        String releaseVersion = releaseVersion();
 
-        Optional<String> buildNumberAsString = extractFinalInt(releaseVersion);
-
-        return buildNumberAsString.flatMap(this::parseInteger);
-    }
-
-    private Optional<String> extractFinalInt(String dotSeparatedIntegers) {
-        int lastDot = dotSeparatedIntegers.lastIndexOf('.');
-
-        if (lastDot == -1 || lastDot == dotSeparatedIntegers.length() - 1) {
-            return Optional.empty();
-        }
-
-        return Optional.of(dotSeparatedIntegers.substring(lastDot + 1));
-    }
-
-    private Optional<Integer> parseInteger(String string) {
-        try {
-            return Optional.of(parseInt(string));
-        } catch (NumberFormatException e) {
-            return Optional.empty();
-        }
-    }
 
     @Override
     public String toString() {
