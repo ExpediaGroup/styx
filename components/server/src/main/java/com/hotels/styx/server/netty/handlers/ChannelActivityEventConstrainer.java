81d80d7d0d97eadaf48c0ab2df686ad3dab19d42
Kyle Vosper <kvosper@gmail.com>
2019-01-03 10:33:23 +0000

    channelActivated was not reset

--- components/server/src/main/java/com/hotels/styx/server/netty/handlers/ChannelActivityEventConstrainer.java
+++ components/server/src/main/java/com/hotels/styx/server/netty/handlers/ChannelActivityEventConstrainer.java
@@ -39,5 +39,7 @@ public class ChannelActivityEventConstrainer extends ChannelInboundHandlerAdapte
         if (channelActivated) {
             super.channelInactive(ctx);
         }
+
+        channelActivated = false;
     }
 }
