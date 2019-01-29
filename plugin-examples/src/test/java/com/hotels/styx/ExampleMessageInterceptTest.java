675a669c523e5f0987a80cead6602ab8d337e458
vivian lopes <vivian.lopes@ada.ac.uk>
2019-01-29 08:57:45 +0000

    added comments

---
+++ plugin-examples/src/test/java/com/hotels/styx/ExampleMessageInterceptTest.java
@@ -0,0 +1,34 @@
+package com.hotels.styx;
+
+import com.hotels.styx.api.*;
+import org.testng.annotations.Test;
+import reactor.core.publisher.Mono;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+/**
+ * This tests the behaviours added in the example plugin.
+ */
+
+public class PluginExampleTest {
+    @Test
+    public void returnsEarly() {
+        PluginExample plugin = new PluginExample();
+
+        LiveHttpRequest request = LiveHttpRequest.get("/")
+                .header("X-Respond", "foo")
+                .build();
+        HttpInterceptor.Chain chain = request1 -> Eventual.of(LiveHttpResponse.response().build());
+
+        Eventual<LiveHttpResponse> eventualLive = plugin.intercept(request, chain);
+        Eventual<HttpResponse> eventual = eventualLive.flatMap(response -> response.aggregate(100));
+
+        HttpResponse response = Mono.from(eventual).block();
+
+        assertThat(response.bodyAs(UTF_8), is("Responding from plugin"));
+    }
+
+}
