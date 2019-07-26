c0049863506a919683bee7b85a859129a9903a6e
Kyle Vosper <kvosper@gmail.com>
2019-07-26 11:58:17 +0100

    Add a unit test

---
+++ components/common/src/test/java/com/hotels/styx/api/EarlyReturnTest.java
@@ -0,0 +1,84 @@
+package com.hotels.styx.api;
+
+import org.testng.annotations.Test;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.hotels.styx.api.HttpResponseStatus.OK;
+import static com.hotels.styx.api.LiveHttpRequest.post;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class EarlyReturnTest {
+    @Test
+    public void bodyIsConsumedWhenReturningEarlyWithError() {
+        AtomicBoolean consumed = new AtomicBoolean();
+
+        ByteStream content = ByteStream.from("foo", UTF_8)
+                .doOnEnd(oe -> consumed.set(true));
+
+        LiveHttpRequest request = post("/")
+                .body(content)
+                .build();
+
+        RuntimeException myException = new RuntimeException("This is just a test");
+
+        Eventual<LiveHttpResponse> eventual = EarlyReturn.returnEarlyWithError(request, myException);
+
+        try {
+            Mono.from(eventual).block();
+        } catch (Exception e) {
+            assertThat(e, is(myException));
+        }
+
+        assertThat(consumed.get(), is(true));
+    }
+
+    @Test
+    public void bodyIsConsumedWhenReturningEarlyWithLiveResponse() {
+        AtomicBoolean consumed = new AtomicBoolean();
+
+        ByteStream content = ByteStream.from("foo", UTF_8)
+                .doOnEnd(oe -> consumed.set(true));
+
+        LiveHttpRequest request = post("/")
+                .body(content)
+                .build();
+
+        Eventual<LiveHttpResponse> eventual = EarlyReturn.returnEarlyWithResponse(request, LiveHttpResponse.response(OK)
+                .body(ByteStream.from("Expected response content", UTF_8))
+                .build());
+
+        HttpResponse response = Mono.from(eventual.flatMap(resp ->
+                resp.aggregate(1000)))
+                .block();
+
+        assertThat(response.bodyAs(UTF_8), is("Expected response content"));
+        assertThat(consumed.get(), is(true));
+    }
+
+    @Test
+    public void bodyIsConsumedWhenReturningEarlyWithNonLiveResponse() {
+        AtomicBoolean consumed = new AtomicBoolean();
+
+        ByteStream content = ByteStream.from("foo", UTF_8)
+                .doOnEnd(oe -> consumed.set(true));
+
+        LiveHttpRequest request = post("/")
+                .body(content)
+                .build();
+
+        Eventual<LiveHttpResponse> eventual = EarlyReturn.returnEarlyWithResponse(request, HttpResponse.response(OK)
+                .body("Expected response content", UTF_8)
+                .build());
+
+        HttpResponse response = Mono.from(eventual.flatMap(resp ->
+                resp.aggregate(1000)))
+                .block();
+
+        assertThat(response.bodyAs(UTF_8), is("Expected response content"));
+        assertThat(consumed.get(), is(true));
+    }
+}
