3d3e4bf63f427a641aa385325f53ee7759ac0e71
Robert Macaulay <rmacaulay@homeaway.com>
2019-10-08 14:37:03 -0500

    Make admin interfaces streamable

---
+++ components/server/src/main/java/com/hotels/styx/server/AdminHttpRouter.java
@@ -0,0 +1,54 @@
+/*
+  Copyright (C) 2013-2019 Expedia Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+package com.hotels.styx.server;
+
+import com.hotels.styx.api.Eventual;
+import com.hotels.styx.api.HttpHandler;
+import com.hotels.styx.api.HttpInterceptor;
+import com.hotels.styx.api.LiveHttpRequest;
+import com.hotels.styx.api.LiveHttpResponse;
+import com.hotels.styx.api.WebServiceHandler;
+import com.hotels.styx.common.http.handler.HttpAggregator;
+
+import static com.hotels.styx.api.HttpResponse.response;
+import static com.hotels.styx.api.HttpResponseStatus.NOT_FOUND;
+
+/**
+ * Simple Http Router.
+ */
+public class AdminHttpRouter implements HttpHandler {
+    private static final int MEGABYTE = 1024 * 1024;
+    private static final HttpHandler NOT_FOUND_HANDLER = (request, context) -> Eventual.of(response(NOT_FOUND).build().stream());
+
+    private final PathTrie<HttpHandler> routes = new PathTrie<>();
+
+    @Override
+    public Eventual<LiveHttpResponse> handle(LiveHttpRequest request, HttpInterceptor.Context context) {
+        return routes.get(request.path())
+            .orElse(NOT_FOUND_HANDLER)
+            .handle(request, context);
+    }
+
+    public AdminHttpRouter aggregate(String path, WebServiceHandler httpHandler) {
+        routes.put(path, new HttpAggregator(MEGABYTE, httpHandler));
+        return this;
+    }
+
+    public AdminHttpRouter stream(String path, HttpHandler httpHandler) {
+        routes.put(path, httpHandler);
+        return this;
+    }
+}
