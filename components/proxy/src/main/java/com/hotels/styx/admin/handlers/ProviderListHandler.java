4c15d3a4a04c18d6baac8404f4d669b9fee94947
Chris Gresty <a-cgresty@hotels.com>
2019-12-04 17:33:19 +0000

    First cut of provider endpoint admin page

---
+++ components/proxy/src/main/java/com/hotels/styx/admin/handlers/ProviderListHandler.java
@@ -0,0 +1,85 @@
+/*
+  Copyright (C) 2013-2019 Expedia Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+package com.hotels.styx.admin.handlers;
+
+
+import com.hotels.styx.api.Eventual;
+import com.hotels.styx.api.HttpInterceptor;
+import com.hotels.styx.api.HttpRequest;
+import com.hotels.styx.api.HttpResponse;
+import com.hotels.styx.api.WebServiceHandler;
+import com.hotels.styx.routing.db.StyxObjectStore;
+import com.hotels.styx.routing.handlers.ProviderObjectRecord;
+
+import java.util.stream.Collectors;
+
+import static com.google.common.net.MediaType.HTML_UTF_8;
+import static com.hotels.styx.admin.AdminServerBuilder.adminEndpointPath;
+import static com.hotels.styx.admin.AdminServerBuilder.adminPath;
+import static com.hotels.styx.api.HttpHeaderNames.CONTENT_TYPE;
+import static com.hotels.styx.api.HttpResponse.response;
+import static com.hotels.styx.api.HttpResponseStatus.OK;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * asdasd.
+ */
+public class ProviderListHandler implements WebServiceHandler {
+
+    private final StyxObjectStore<ProviderObjectRecord> providerDb;
+
+    public ProviderListHandler(StyxObjectStore<ProviderObjectRecord> providerDb) {
+        this.providerDb = providerDb;
+    }
+
+    @Override
+    public Eventual<HttpResponse> handle(HttpRequest request, HttpInterceptor.Context context) {
+        String output = providerDb.entrySet().stream()
+                .map(entry -> htmlForProvider(entry.getKey(), entry.getValue()))
+                .collect(Collectors.joining("\n"));
+        return Eventual.of(response(OK)
+                .body(output, UTF_8)
+                .addHeader(CONTENT_TYPE, HTML_UTF_8.toString())
+                .build());
+    }
+
+    private String htmlForProvider(String name, ProviderObjectRecord provider) {
+        StringBuilder html = new StringBuilder();
+        html.append(h3(name + " (" + provider.getType() + ")"));
+        html.append("<ul>");
+        html.append(provider.getStyxService()
+                .adminInterfaceHandlers(adminPath("providers", name))
+                .keySet()
+                .stream()
+                .map(relativePath -> adminEndpointPath("providers", name, relativePath))
+                .map(absolutePath -> li(link(absolutePath, absolutePath)))
+                .collect(Collectors.joining("\n")));
+        html.append("</ul>");
+        return html.toString();
+    }
+
+    private String h3(String content) {
+        return String.format("<h3>%s</h3>\n", content);
+    }
+
+    private String link(String href, String text) {
+        return String.format("<a href=\"%s\">%s</a>", href, text);
+    }
+
+    private String li(String content) {
+        return String.format("<li>%s</li>\n", content);
+    }
+}
