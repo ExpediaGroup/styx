27feb538483af1dafa4e4b406e97e0faca30a792
David Latorre <dvlato@gmail.com>
2020-02-10 11:17:19 +0000

    Add support for the SameSite cookie attribute (closes #604)  (#607)

--- components/api/src/main/java/com/hotels/styx/api/HttpVersion.java
+++ components/api/src/main/java/com/hotels/styx/api/HttpVersion.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2018 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -40,12 +40,12 @@ public class HttpVersion {
 
     /**
      * Creates a HttpVersion from String.
-     *
+     * <p>
      * Accepted strings are "HTTP/1.0" and "HTTP/1.1".
      * Otherwise throws an {@link IllegalArgumentException}.
      *
      * @param version
-     * @return
+     * @return HttpVersion for the received version
      */
     public static HttpVersion httpVersion(String version) {
         checkArgument(VERSIONS.containsKey(version), "No such HTTP version %s", version);
