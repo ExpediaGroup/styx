ExpediaGroup/styx

commit 8b0ba38b5d5f945cc3c07e2983be6a6b8b8d6675

Resolve rendering issues with YamlFileCofigurationService origin page (#648)

components/proxy/src/main/kotlin/com/hotels/styx/services/OriginsPageRenderer.kt
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2019 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 package com.hotels.styx.services
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.NullNode
 import com.hotels.styx.infrastructure.configuration.yaml.JsonNodeConfig
 import com.hotels.styx.lbGroupTag
 import com.hotels.styx.routing.RoutingObjectRecord
@@ -36,16 +37,12 @@ import kotlinx.html.dom.document
 import kotlinx.html.dom.serialize
 import kotlinx.html.h3
 import kotlinx.html.h6
-import kotlinx.html.head
-import kotlinx.html.html
 import kotlinx.html.link
 import kotlinx.html.meta
-import kotlinx.html.script
 import kotlinx.html.span
 import kotlinx.html.table
 import kotlinx.html.tbody
 import kotlinx.html.td
-import kotlinx.html.title
 import kotlinx.html.tr
 
 internal class OriginsPageRenderer(val assetsRoot: String, val provider: String, val routeDatabase: StyxObjectStore<RoutingObjectRecord>) {
@@ -253,5 +250,8 @@ internal class OriginsPageRenderer(val assetsRoot: String, val provider: String,
             .filterNot { it.startsWith("state:inactive") }
             .filter { lbGroupTag.valueOf(it) == null }
 
-    private fun isSecureHost(config: JsonNode) = config.get("tlsSettings") != null
+    private fun isSecureHost(config: JsonNode): Boolean {
+        val tlsSettings = config.get("tlsSettings")
+        return tlsSettings != null && tlsSettings !is NullNode
+    }
 }

components/proxy/src/main/kotlin/com/hotels/styx/routing/RoutingObjectRecord.kt
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2019 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -30,13 +30,13 @@ internal data class RoutingObjectRecord(
     companion object {
         fun create(type: String, tags: Set<String>, config: JsonNode, routingObject: RoutingObject) = RoutingObjectRecord(
                 type,
-                tags + "created:${timestamp()}",
+                tags + "created=${timestamp()}",
                 config,
                 RoutingMetadataDecorator(routingObject))
     }
 
     fun creationTime() = tags
-            .filter { it.startsWith("created:") }
+            .filter { it.startsWith("created=") }
             .first()
 }
 

components/proxy/src/main/kotlin/com/hotels/styx/services/YamlFileConfigurationService.kt
@@ -101,7 +101,7 @@ internal class YamlFileConfigurationService(
             }
 
     override fun adminInterfaceHandlers(namespace: String) = mapOf(
-            "assets/" to HttpAggregator(ClassPathResourceHandler("$namespace/assets/", "/admin/assets/YamlConfigurationService")),
+            "assets/.*" to HttpAggregator(ClassPathResourceHandler("$namespace/assets/", "/admin/assets/YamlConfigurationService")),
             "configuration" to HttpContentHandler(PLAIN_TEXT_UTF_8.toString(), UTF_8) { originsConfig },
             "origins" to HttpContentHandler(HTML_UTF_8.toString(), UTF_8) {
                 OriginsPageRenderer("$namespace/assets", name, routeDb).render()

commit 594098488b9a11ed383b215557072ce22ba38fcb

Fix the broken build caused by PR #648. (#651)
Update copyright headers.

components/proxy/src/main/java/com/hotels/styx/StyxConfig.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2019 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

components/proxy/src/main/java/com/hotels/styx/infrastructure/logging/ExceptionConverter.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2018 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

components/proxy/src/main/java/com/hotels/styx/proxy/encoders/ConfigurableUnwiseCharsEncoder.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2018 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

components/proxy/src/main/java/com/hotels/styx/proxy/interceptors/ViaHeaderAppendingInterceptor.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2018 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

components/proxy/src/main/kotlin/com/hotels/styx/services/OriginsPageRenderer.kt
@@ -37,12 +37,16 @@ import kotlinx.html.dom.document
 import kotlinx.html.dom.serialize
 import kotlinx.html.h3
 import kotlinx.html.h6
+import kotlinx.html.head
+import kotlinx.html.html
 import kotlinx.html.link
 import kotlinx.html.meta
+import kotlinx.html.script
 import kotlinx.html.span
 import kotlinx.html.table
 import kotlinx.html.tbody
 import kotlinx.html.td
+import kotlinx.html.title
 import kotlinx.html.tr
 
 internal class OriginsPageRenderer(val assetsRoot: String, val provider: String, val routeDatabase: StyxObjectStore<RoutingObjectRecord>) {

components/server/src/main/java/com/hotels/styx/server/routing/antlr/ExpressionVisitor.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2018 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

support/test-api/src/main/java/com/hotels/styx/testapi/ssl/SslTesting.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2018 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

components/proxy/src/test/kotlin/com/hotels/styx/routing/RoutingObjectRecordTest.kt
@@ -21,7 +21,7 @@ import io.mockk.mockk
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter.ISO_DATE_TIME
 
-private const val CREATED_STRING = "created:"
+private const val CREATED_STRING = "created="
 private const val TIMESTAMP_START_POSITION = CREATED_STRING.length
 
 class RoutingObjectRecordTest : StringSpec({

components/proxy/src/test/kotlin/com/hotels/styx/services/HealthCheckMonitoringServiceTest.kt
@@ -42,7 +42,7 @@ import java.util.concurrent.TimeUnit.MILLISECONDS
 class HealthCheckMonitoringServiceTest : FeatureSpec({
     val LOGGER = LoggerFactory.getLogger(HealthCheckMonitoringServiceTest::class.java)
 
-    fun createdTag(tag: String) = tag.matches("created:.*".toRegex())
+    fun createdTag(tag: String) = tag.matches("created=.*".toRegex())
 
     val workerExecutor = NettyExecutor.create("monitoringServiceTest", 1)
 

components/server/src/test/java/com/hotels/styx/server/handlers/NettyHttpResponseContentMatcher.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2019 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

system-tests/e2e-suite/src/test/java/com/hotels/styx/BuildVsRuntimeJarAccessTest.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2019 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

system-tests/e2e-testsupport/src/main/java/com/hotels/styx/utils/handlers/ContentDigestHandler.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2019 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

system-tests/styx-test-plugin/src/main/java/testgrp/TestPlugin.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2019 Expedia Inc.
+  Copyright (C) 2013-2020 Expedia Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.