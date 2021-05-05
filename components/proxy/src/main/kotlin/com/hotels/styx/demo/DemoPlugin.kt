65e5395a04ff97765aa75b34ea91608913652449
Kyle Vosper <kvosper@expedia.com>
2021-05-05 16:52:02 +0100

    Refactored code a little

--- components/proxy/src/main/kotlin/com/hotels/styx/demo/DemoPlugin.kt
+++ components/proxy/src/main/kotlin/com/hotels/styx/demo/DemoPlugin.kt
@@ -20,7 +20,6 @@ import com.hotels.styx.api.plugins.spi.Plugin
 import com.hotels.styx.api.plugins.spi.PluginFactory
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.nio.charset.StandardCharsets
 import java.nio.charset.StandardCharsets.UTF_8
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -32,41 +31,45 @@ class DemoPlugin : Plugin {
         logger.info("Demo plugin initialised")
     }
 
-    override fun adminInterfaceHandlers(): Map<String, HttpHandler> {
-        return mapOf(
-            "hits" to HttpHandler { request, context ->
-                Eventual.of(
-                    LiveHttpResponse.response()
-                        .body(ByteStream.from("Hits: ${hits.get()}", UTF_8))
-                        .build()
-                )
-            }
-        )
-    }
+    override fun adminInterfaceHandlers(): Map<String, HttpHandler> = mapOf(
+        "hits" to respondWith {
+            "Hits: ${hits.get()}"
+        }
+    )
 
     override fun intercept(request: LiveHttpRequest, chain: HttpInterceptor.Chain): Eventual<LiveHttpResponse> {
         logger.info("Demo plugin received request $request")
         hits.incrementAndGet()
 
-        val exceptionQueryParam = request.queryParam("exception")
-
-        exceptionQueryParam.filter {
-            it.equals("onRequest", true)
-        }.ifPresent {
+        if (request.askedForExceptionOnRequest()) {
             throw DemoPluginException("Demo Plugin: Deliberate exception on request")
         }
 
         return chain.proceed(request).map { response ->
-            exceptionQueryParam.filter {
-                it.equals("onResponse", false)
-            }.ifPresent {
+            if (request.askedForExceptionOnResponse())
                 throw DemoPluginException("Demo Plugin: Deliberate exception on response")
-            }
-
-            response
+            else
+                response
         }
     }
 }
+
+private fun respondWith(content: () -> String) = handler {
+    body(content(), UTF_8)
+}
+
+private fun handler(lambda: HttpResponse.Builder.() -> Unit) = HttpHandler { _, _ ->
+    val builder = HttpResponse.response()
+    builder.lambda()
+
+    Eventual.of(builder.build().stream())
+}
+
+private fun LiveHttpRequest.exceptionParam() = queryParam("exception").orElse(null)
+private fun LiveHttpRequest.askedForExceptionOnRequest() = exceptionParam() matches "onRequest"
+private fun LiveHttpRequest.askedForExceptionOnResponse() = exceptionParam() matches "onResponse"
+
+private infix fun String?.matches(value: String) = this?.equals(value, true) ?: false
 
 class DemoPluginFactory : PluginFactory {
     override fun create(environment: PluginFactory.Environment) = DemoPlugin()
